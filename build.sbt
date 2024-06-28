6cafb8a84bf6ad40e8d8e7d3caed2889e217c69e
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-06-28 16:29:12 +0100

    Add CLI

--- build.sbt
+++ build.sbt
@@ -6,13 +6,14 @@ ThisBuild / organizationName := "jcazevedo"
 
 lazy val root = (project in file("."))
   .settings(
-    name              := "icfpc2024",
-    scalafmtOnCompile := true,
-    semanticdbEnabled := true,
-    semanticdbVersion := scalafixSemanticdb.revision,
-    scalafixOnCompile := true,
-    fork              := true,
+    name                             := "icfpc2024",
+    scalafmtOnCompile                := true,
+    semanticdbEnabled                := true,
+    semanticdbVersion                := scalafixSemanticdb.revision,
+    scalafixOnCompile                := true,
+    fork                             := true,
     libraryDependencies ++= Seq(
+      JLine,
       Log4jCore,
       Log4jSlf4jImpl,
       PekkoActor,
@@ -24,5 +25,12 @@ lazy val root = (project in file("."))
       PureConfigPekkoHttp,
       ScalaLogging,
       MUnit % Test
-    )
+    ),
+    assembly / mainClass             := Some("net.jcazevedo.icfpc2024.CLI"),
+    assembly / assemblyJarName       := name.value + "-" + version.value + ".jar",
+    assembly / assemblyMergeStrategy := {
+      case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+      case "reference.conf"                    => MergeStrategy.concat
+      case _                                   => MergeStrategy.first
+    }
   )
---
+++ src/main/scala/net/jcazevedo/icfpc2024/CLI.scala
@@ -0,0 +1,62 @@
+package net.jcazevedo.icfpc2024
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
+
+import org.apache.pekko.actor.ActorSystem
+import org.jline.reader.impl.DefaultParser
+import org.jline.reader.{EndOfFileException, LineReader, LineReaderBuilder, UserInterruptException}
+import org.jline.terminal.{Terminal, TerminalBuilder}
+
+import net.jcazevedo.icfpc2024.api.APIClient
+import net.jcazevedo.icfpc2024.config.Config
+import net.jcazevedo.icfpc2024.lang.Strings
+
+object CLI extends App {
+  val config: Config =
+    Config.load
+
+  val terminal: Terminal =
+    TerminalBuilder.terminal()
+
+  val reader: LineReader =
+    LineReaderBuilder.builder().terminal(terminal).build()
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContext = system.dispatcher
+  val client = new APIClient(config.uri, config.headers)
+
+  def processLine(line: String): Unit = {
+    val result = Strings.fromAlien(client.postSync(s"S${Strings.fromHuman(line)}").tail)
+    println(result)
+  }
+
+  def loop(): Unit = {
+    var more: Boolean = true
+
+    while (more) {
+      val line: String =
+        try {
+          reader.readLine("icfpc2024> ")
+        } catch {
+          case _: UserInterruptException =>
+            println("Press Control-D to exit")
+            ""
+
+          case _: EndOfFileException => return
+        }
+
+      if (line != null) {
+        val trimmed = line.trim
+
+        if (trimmed.equalsIgnoreCase("quit") || trimmed.equalsIgnoreCase("quit"))
+          more = false
+        else
+          processLine(line)
+      }
+    }
+  }
+
+  loop()
+  Await.ready(system.terminate(), Duration.Inf)
+}
