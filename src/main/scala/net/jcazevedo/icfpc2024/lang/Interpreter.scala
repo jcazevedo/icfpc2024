e6072d9a3b25830651b424479b3c3760ede00527
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-06-29 02:52:54 +0100

    Improve lambda application (still buggy)

--- src/main/scala/net/jcazevedo/icfpc2024/lang/Interpreter.scala
+++ src/main/scala/net/jcazevedo/icfpc2024/lang/Interpreter.scala
@@ -18,17 +18,28 @@ object Interpreter {
   def error(value: ICFP): Exception =
     new IllegalArgumentException(s"Unexpected value $value")
 
-  sealed trait Result
-  case class Final(res: ICFP.Atom) extends Result
-  case class More(f: PartialFunction[ICFP.Atom, Result], skip: Int = 0) extends Result
+  def error(operator: ICFP.Operator): Exception =
+    new IllegalArgumentException(s"Unexpected operator $operator")
+
+  sealed trait ContextOp
+  case object Empty extends ContextOp
+  case class PushBinding(expr: ICFP.Atom) extends ContextOp
+  case object PopBinding extends ContextOp
+  case class PushContext(variable: Long) extends ContextOp
+  case object PopContext extends ContextOp
+
+  sealed trait Operation {
+    def contextOp: ContextOp
+  }
+  case class Contextual(contextOp: ContextOp) extends Operation
+  case class Final(res: ICFP.Atom, contextOp: ContextOp = Empty) extends Operation
+  case class More(f: PartialFunction[ICFP.Atom, Operation], skip: Int = 0, contextOp: ContextOp = Empty)
+      extends Operation
 
   def opResult(operator: Operator): More = {
     operator match {
       case Drop =>
         More({ case ICFP.Integer(x) => More({ case ICFP.String(y) => Final(ICFP.String(y.drop(x.toInt))) }) })
-
-      case LambdaApplication =>
-        ???
 
       case IsEqual =>
         More({ case x => More({ case y => Final(ICFP.Boolean(x == y)) }) })
@@ -75,9 +86,6 @@ object Interpreter {
       case Negate =>
         More({ case ICFP.Integer(value) => Final(ICFP.Integer(-value)) })
 
-      case LambdaAbstraction(variableNumber) =>
-        ???
-
       case Not =>
         More({ case ICFP.Boolean(value) => Final(ICFP.Boolean(!value)) })
 
@@ -89,61 +97,106 @@ object Interpreter {
 
       case StringToInt =>
         More({ case str: ICFP.String => Final(ICFP.Integer(ICFP.Integer.fromBase94(str.toString().stripPrefix("S")))) })
+
+      case other =>
+        throw error(other)
     }
   }
 
   def evaluate(expression: ICFP): ICFP.Atom = {
-    val operators = mutable.Stack.empty[More]
+    val operations = mutable.Stack.empty[More]
     val expressions = mutable.Stack.empty[ICFP]
     val operandsInStack = mutable.Stack.empty[Int]
+    val bindings = mutable.Stack.empty[ICFP]
+    val context = mutable.Stack.empty[Map[Long, ICFP]]
     expressions.push(expression)
-    operators.push(More({ case value => Final(value) }))
+    operations.push(More({ case value => Final(value) }))
     operandsInStack.push(1)
+    context.push(Map.empty)
 
-    while (operators.nonEmpty) {
-      println("operators:")
-      println(operators)
-      println("expressions:")
-      println(expressions)
+    def applyContextOp(contextOp: ContextOp): Unit =
+      contextOp match {
+        case Empty                 => // do nothing
+        case PushBinding(expr)     => bindings.push(expr)
+        case PopBinding            => bindings.pop()
+        case PushContext(variable) => context.push(context.top + (variable -> bindings.top))
+        case PopContext            => context.pop()
+      }
 
-      val More(f, skip) = operators.top
+    while (operations.nonEmpty) {
+      // println("operators:")
+      // println(operations)
+      // println("expressions:")
+      // println(expressions)
+      // println("bindings:")
+      // println(bindings)
+      // println("context:")
+      // println(context)
 
-      if (expressions.top.isAtom) {
-        operators.pop()
-        val operands = operandsInStack.pop()
+      val More(f, skip, contextOp) = operations.top
+      if (skip > 0) {
         (0 until skip).foreach(_ => expressions.pop())
+        operations.pop()
+        operations.push(More(f, 0, contextOp))
+        val operands = operandsInStack.pop()
+        operandsInStack.push(operands - skip)
+      } else if (expressions.top.isAtom) {
+        val More(f, _, contextOp) = operations.pop()
+        applyContextOp(contextOp)
+        val operands = operandsInStack.pop()
         val expr = expressions.pop().asInstanceOf[ICFP.Atom]
         f(expr) match {
-          case Final(res) =>
-            (1 until (operands - skip)).foreach(_ => expressions.pop())
+          case Final(res, contextOp) =>
+            applyContextOp(contextOp)
+            (1 until operands).foreach(_ => expressions.pop())
             expressions.push(res)
 
+          case Contextual(contextOp) =>
+            applyContextOp(contextOp)
+
           case more: More =>
-            operators.push(more)
-            operandsInStack.push(operands - skip - 1)
+            operations.push(more)
+            operandsInStack.push(operands - 1)
         }
       } else {
         expressions.pop() match {
-          case Unary(operator, expr) =>
-            operators.push(opResult(operator))
+          // Lambda absractions have a special treatment.
+          case Unary(LambdaAbstraction(variable), expr) =>
+            context.push(context.top + (variable -> bindings.top))
+            operations.push(More({ case atom => Final(atom, contextOp = PopContext) }))
             expressions.push(expr)
             operandsInStack.push(1)
 
+          case Unary(operator, expr) =>
+            operations.push(opResult(operator))
+            expressions.push(expr)
+            operandsInStack.push(1)
+
+          // Lambda applications have a special treatment.
+          case Binary(LambdaApplication, lhs, rhs) =>
+            operations.push(More({ case atom => Final(atom, contextOp = PopBinding) }))
+            expressions.push(lhs)
+            operandsInStack.push(1)
+            bindings.push(rhs)
+          // operations.push(More({ case atom => Contextual(contextOp = PushBinding(atom)) }))
+          // expressions.push(rhs)
+          // operandsInStack.push(1)
+
           case Binary(operator, lhs, rhs) =>
-            operators.push(opResult(operator))
+            operations.push(opResult(operator))
             expressions.push(rhs)
             expressions.push(lhs)
             operandsInStack.push(2)
 
           case Ternary(operator, expr1, expr2, expr3) =>
-            operators.push(opResult(operator))
+            operations.push(opResult(operator))
             expressions.push(expr3)
             expressions.push(expr2)
             expressions.push(expr1)
             operandsInStack.push(3)
 
           case Variable(value) =>
-            ???
+            expressions.push(context.top(value))
 
           case other =>
             throw error(other)
