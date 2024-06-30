87c05d2b5c38d3245d178335c14ee715a21d1cea
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-06-30 16:37:31 +0100

    Allow producing JavaScript expressions

--- src/main/scala/net/jcazevedo/icfpc2024/lang/Utilities.scala
+++ src/main/scala/net/jcazevedo/icfpc2024/lang/Utilities.scala
@@ -13,6 +13,63 @@ object Utilities {
       ICFP.Integer(count)
     )
 
+  def asJavaScriptString(expression: ICFP): String =
+    expression match {
+      case ICFP.Boolean(value) =>
+        if (value) "true" else "false"
+
+      case ICFP.Integer(value) =>
+        s"$value"
+
+      case ICFP.String(value) =>
+        s"$value"
+
+      case ICFP.Variable(value) =>
+        s"v$value"
+
+      case ICFP.Expression.Unary(ICFP.Operator.Unary.LambdaAbstraction(variable), expression) =>
+        s"(v$variable => ${asJavaScriptString(expression)})"
+
+      case ICFP.Expression.Unary(operator, expression) =>
+        val opCode = operator match {
+          case ICFP.Operator.Unary.Negate      => "-"
+          case ICFP.Operator.Unary.Not         => "!"
+          case ICFP.Operator.Unary.StringToInt => "stringToInt"
+          case ICFP.Operator.Unary.IntToString => "intToString"
+        }
+
+        s"$opCode(${asJavaScriptString(expression)})"
+
+      case ICFP.Expression.Binary(ICFP.Operator.Binary.LambdaApplication, lhs, rhs) =>
+        s"${asJavaScriptString(lhs)}(${asJavaScriptString(rhs)})"
+
+      case ICFP.Expression.Binary(ICFP.Operator.Binary.Take, lhs, rhs) =>
+        s"${asJavaScriptString(rhs)}.substr(0, ${asJavaScriptString(lhs)})"
+
+      case ICFP.Expression.Binary(ICFP.Operator.Binary.Drop, lhs, rhs) =>
+        s"${asJavaScriptString(rhs)}.substr(${asJavaScriptString(lhs)})"
+
+      case ICFP.Expression.Binary(operator, lhs, rhs) =>
+        val opCode = operator match {
+          case ICFP.Operator.Binary.Add           => "+"
+          case ICFP.Operator.Binary.Subtract      => "-"
+          case ICFP.Operator.Binary.Multiply      => "*"
+          case ICFP.Operator.Binary.Divide        => "/"
+          case ICFP.Operator.Binary.Modulo        => "%"
+          case ICFP.Operator.Binary.IsLessThan    => "<"
+          case ICFP.Operator.Binary.IsGreaterThan => ">"
+          case ICFP.Operator.Binary.IsEqual       => "=="
+          case ICFP.Operator.Binary.Or            => "||"
+          case ICFP.Operator.Binary.And           => "&&"
+          case ICFP.Operator.Binary.Concatenate   => "+"
+        }
+
+        s"(${asJavaScriptString(lhs)} $opCode ${asJavaScriptString(rhs)})"
+
+      case ICFP.Expression.Ternary(ICFP.Operator.Ternary.If, condition, ifTrue, ifFalse) =>
+        s"(${asJavaScriptString(condition)} ? ${asJavaScriptString(ifTrue)} : ${asJavaScriptString(ifFalse)})"
+    }
+
   def asLispyString(expression: ICFP): String =
     expression match {
       case ICFP.Boolean(value) =>
