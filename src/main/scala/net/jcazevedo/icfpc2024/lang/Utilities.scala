15db5fa56539dbd2fb3ffd22dff65d597af67966
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-06-30 14:12:26 +0100

    Add a method to convert an ICFP to a lispy syntax

--- src/main/scala/net/jcazevedo/icfpc2024/lang/Utilities.scala
+++ src/main/scala/net/jcazevedo/icfpc2024/lang/Utilities.scala
@@ -12,4 +12,55 @@ object Utilities {
       Parser.parse(s"""L! L" ? B= v" I! S B. ${ICFP.String(str).asString} B$$ v! B- v" I""""),
       ICFP.Integer(count)
     )
+
+  def asLispyString(expression: ICFP): String =
+    expression match {
+      case ICFP.Boolean(value) =>
+        if (value) "true" else "false"
+
+      case ICFP.Integer(value) =>
+        s"$value"
+
+      case ICFP.String(value) =>
+        s"$value"
+
+      case ICFP.Variable(value) =>
+        s"v$value"
+
+      case ICFP.Expression.Unary(operator, expression) =>
+        val opCode = operator match {
+          case ICFP.Operator.Unary.Negate                      => "-"
+          case ICFP.Operator.Unary.Not                         => "!"
+          case ICFP.Operator.Unary.StringToInt                 => "string-to-int"
+          case ICFP.Operator.Unary.IntToString                 => "int-to-string"
+          case ICFP.Operator.Unary.LambdaAbstraction(variable) => s"v$variable =>"
+        }
+
+        s"($opCode ${asLispyString(expression)})"
+
+      case ICFP.Expression.Binary(ICFP.Operator.Binary.LambdaApplication, lhs, rhs) =>
+        s"(${asLispyString(lhs)} ${asLispyString(rhs)})"
+
+      case ICFP.Expression.Binary(operator, lhs, rhs) =>
+        val opCode = operator match {
+          case ICFP.Operator.Binary.Add           => "+"
+          case ICFP.Operator.Binary.Subtract      => "-"
+          case ICFP.Operator.Binary.Multiply      => "*"
+          case ICFP.Operator.Binary.Divide        => "/"
+          case ICFP.Operator.Binary.Modulo        => "%"
+          case ICFP.Operator.Binary.IsLessThan    => "<"
+          case ICFP.Operator.Binary.IsGreaterThan => ">"
+          case ICFP.Operator.Binary.IsEqual       => "="
+          case ICFP.Operator.Binary.Or            => "or"
+          case ICFP.Operator.Binary.And           => "and"
+          case ICFP.Operator.Binary.Concatenate   => "concat"
+          case ICFP.Operator.Binary.Take          => "take"
+          case ICFP.Operator.Binary.Drop          => "drop"
+        }
+
+        s"($opCode ${asLispyString(lhs)} ${asLispyString(rhs)})"
+
+      case ICFP.Expression.Ternary(ICFP.Operator.Ternary.If, condition, ifTrue, ifFalse) =>
+        s"(if ${asLispyString(condition)} ${asLispyString(ifTrue)} ${asLispyString(ifFalse)})"
+    }
 }
